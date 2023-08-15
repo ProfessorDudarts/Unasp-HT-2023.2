b28070f96f74ac882c1a1852f585de7e6033a268
ProfessorDudarts <112520943+ProfessorDudarts@users.noreply.github.com>
2023-08-15 18:01:15 +0000

    Resolve conflitos do Pylint

---
+++ algoritmos_II/exercicio_2_9.py
@@ -0,0 +1,7 @@
+'''
+Faça um programa em Python que calcule a área de um quadrado.
+'''
+
+lado = int(input('Digite o lado do Quadrado: '))
+area = lado ** 2
+print(f'A área do Quadrado é igual a {area}')
