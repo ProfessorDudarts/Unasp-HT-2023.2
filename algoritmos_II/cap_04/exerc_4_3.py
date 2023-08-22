7d886b9128ff387cf503f267880276a46ddf3fc0
ProfessorDudarts <112520943+ProfessorDudarts@users.noreply.github.com>
2023-08-22 23:49:30 +0000

    exercicios 22.08.2023

---
+++ algoritmos_II/cap_04/exerc_4_3.py
@@ -0,0 +1,7 @@
+def exercicio_4_3(numero):
+    print(f'numero = {numero}')
+    print(f'{numero}² = {numero ** 2}')
+    print(f'{numero}³ = {numero ** 3}')
+
+valor = int(input('Digite um valor: '))
+exercicio_4_3(valor)
