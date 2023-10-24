8873ccf1a1d9d981d5954ea481e95c023bd607ac
ProfessorDudarts <112520943+ProfessorDudarts@users.noreply.github.com>
2023-10-24 20:08:53 +0000

    Adicionando teste unitário

--- algoritmos_II/testes.py
+++ algoritmos_II/testes.py
@@ -1,10 +1,24 @@
-from enum import Enum
+import unittest
 
-class LanguagaID(Enum):
-    cobol = 1
-    jcl = 2
-    flowchart = 3
-    spark = 4
-    java = 5
+# A função que você deseja testar
+def calcular_media(numeros):
+    if not numeros:
+        return 0
+    soma = sum(numeros)
+    media = soma / len(numeros)
+    return media
 
-print(LanguagaID.cobol.value)
+def test_media_lista_vazia():
+    assert calcular_media([]) == 0
+
+def test_media_lista_um_elemento():
+    assert calcular_media([5]) == 5.0
+    print('ok')
+
+def test_media_lista_multiplos_elementos():
+    assert calcular_media([1, 2, 3, 4, 5]) == 3.0
+
+if __name__ == '__main__':
+    test_media_lista_vazia()
+    test_media_lista_um_elemento()
+    test_media_lista_multiplos_elementos()
